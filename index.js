4afd9eed154afcacbfe65df624129f83c80b919c
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-07-07 12:04:22 +0200

    return console.log equal undifined

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ const getPrice = (numberOfTickets, hasPopcorn, hasDiscount) => {
 
   return price;
 };
+///////////////// Conditions//////////////
+
+const isGoldenHour = (hour) => {
+    return (hour >= 5 && hour <= 6 ) || (hour >= 18 && hour <= 19 )
+    
+}
+
+const hotOrCold = (color) => {
+  if (color === 'vert' || color === 'bleu' || color === 'indigo' || color === 'violet') {
+    return 'cold';
+  } else if (color === 'jaune' || color === 'rouge' || color === 'orange') {
+    return 'hot';
+  }
+};
